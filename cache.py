cb1261f95df32ee80c35a2c8fcf7115c7cb8e101
Leonardo Ventura <leonardo.venturacl@gmail.com>
2021-06-09 06:14:45 -0300

    Adiciona tempo de resposta

--- cache.py
+++ cache.py
@@ -5,7 +5,7 @@ from util import write_log
 
 # [ref] heapq: https://docs.python.org/3/library/heapq.html#basic-examples
 class Cache:
-    def __init__(self, id, events, size=2):
+    def __init__(self, id, events, size=2, theta=float('inf'), mu=1):
         ''' Inicializa cache
         
         Parametros:
@@ -18,7 +18,8 @@ class Cache:
         self._events = events
         hq.heapify(self._events)
         self._size = size
-        self._last_arrival = 0
+        self.theta = theta
+        self.mu = mu
         self.misses = {}
 
     def next_execution(self):
@@ -28,37 +29,47 @@ class Cache:
         for event in self._events:
             yield event[1]
 
+    def _handle_empty(self, event):
+        hq.heappush(self._events, event)
+        return 0
+
     def _handle_hit(self, event):
         ''' Função a ser implementa nas caches que herdam a cache principal'''
-        return
+        return 0
 
     def _handle_miss(self, event):
         ''' Função a ser implementa nas caches que herdam a cache principal'''
-        return
+        return 0
 
     def add(self, event, cid, n_pending_requests, p=1):
         ''' Adiciona evento (arrival_time, content) a cache'''
         arrival_time, content = event
+        response_time = 0
         if np.random.random() <= p:
             if len(self._events) == self._size:
                 if content not in self.events():
                     request_type = 'miss'
                     self.misses[cid] = 1
                     self._handle_miss(event)
+                    response_time = np.random.exponential(1/self.theta)
                 else:
                     request_type = 'hit'
                     self.misses[cid] = 0
                     self._handle_hit(event)
+                    response_time = 0
 
             else:
                 request_type = 'miss'
                 self.misses[cid] = 1
-                hq.heappush(self._events, event)
+                self._handle_empty(event)
+                response_time = 0
         else:
             request_type = 'miss'
             self.misses[cid] = 1
 
+        downstream_time = np.random.exponential(1/self.mu)
+        response_time += downstream_time
         write_log(tempo=arrival_time, cache=self.id, cid=cid,
             tipo_requisicao=request_type, n_requisicoes_pendentes=n_pending_requests)
 
-        self._last_arrival = max(self._last_arrival, event[0])
+        return response_time
