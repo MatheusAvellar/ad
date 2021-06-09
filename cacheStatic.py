20c9d12d62d5520d84be420b9b887a3da696206d
Leonardo Ventura <leonardo.venturacl@gmail.com>
2021-06-09 04:20:57 -0300

    Adiciona cache estática

---
+++ cacheStatic.py
@@ -0,0 +1,11 @@
+import numpy as np
+import heapq as hq
+
+from cache import Cache
+
+class CacheRandom(Cache):
+    def _handle_hit(self, event):
+        return
+
+    def _handle_miss(self, event):
+        return
